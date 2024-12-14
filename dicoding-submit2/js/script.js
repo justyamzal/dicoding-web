5b7154ae04619dca619af6414cc4e213c1859734
justyamzal <43919037+yamzal@users.noreply.github.com>
2024-12-14 09:17:36 +0700

    card slider adding

--- dicoding-submit2/js/script.js
+++ dicoding-submit2/js/script.js
@@ -8,3 +8,36 @@
         nav.classList.toggle('collapse'); // Toggle the 'collapse' class
     });
  });
+
+
+new Swiper('.card-wrapper', {
+    loop: true,
+    spaceBetween:30,
+    
+    // If we need pagination, pagination bullets
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // Responsive breakpoint
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1200: {
+            slidesPerView: 4
+        }
+    }
+
+  });
